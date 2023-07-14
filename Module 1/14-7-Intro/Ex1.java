7d56a6073d9ee232b1a98cf18ce43a7ad6b5561a
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-07-14 12:35:57 +0530

    Add files via upload

---
+++ Module 1/14-7-Intro/Ex1.java
@@ -0,0 +1,12 @@
+package com.a147;
+
+public class Ex1 
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println("Abcd");
+	
+	}
+	
+}
---
+++ Module 1/14-7-Intro/HelloEx.java
@@ -0,0 +1,20 @@
+package com.a147;
+
+public class HelloEx 
+{
+	
+	//void - return type - null
+	//main - method name 
+	//() - Methods 
+	//static - fixed
+	
+	public static void main(String tops[])
+	{
+		
+		System.out.println("Hello World");
+		
+	}
+	
+	
+	
+}
